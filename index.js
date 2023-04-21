d9c5959be319a2d0ccbc3e1eaa530fa8020b6f69
Arifprogrammer <arifmgtcu@gmail.com>
2023-04-22 02:25:27 +0600

    created server for categories

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const categories = require("./data/categories.json");
+var cors = require("cors");
+
+app.use(cors());
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
