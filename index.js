32389550c2806b53e7bc4f0c2dc3ae5690c1b503
Arifprogrammer <arifmgtcu@gmail.com>
2023-04-28 23:58:53 +0600

    server setup for specific news and categories

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const categories = require("./data/categories.json");
-var cors = require("cors");
+const news = require("./data/news.json");
+const cors = require("cors");
 
 app.use(cors());
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news);
+  } else {
+    const category = news.filter((n) => n.category_id === id);
+    res.send(category);
+  }
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
